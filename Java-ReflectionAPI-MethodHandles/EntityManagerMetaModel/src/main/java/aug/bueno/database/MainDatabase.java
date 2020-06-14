eebb270df599f3f080a9324124c921aeeac71074
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2020-06-14 00:05:04 -0300

    TODO H2 db

--- Java-ReflectionAPI-MethodHandles/EntityManagerMetaModel/src/main/java/aug/bueno/database/MainDatabase.java
+++ Java-ReflectionAPI-MethodHandles/EntityManagerMetaModel/src/main/java/aug/bueno/database/MainDatabase.java
@@ -5,7 +5,12 @@ import org.h2.tools.Server;
 import java.sql.SQLException;
 
 public class MainDatabase {
+
     public static void main(String[] args) throws SQLException {
+
+        //TODO https://www.javaguides.net/2019/08/java-h2-database-tutorial.html
+        //TODO https://www.tutorialspoint.com/h2_database/h2_database_jdbc_connection.html
+
         Server.main();
         System.out.println("DB H2 Launched");
     }
